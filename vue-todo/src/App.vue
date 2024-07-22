82e6188469a545cbc23291df2e4297a4c7f3596c
Jared Ledbetter <jared.ledbetter@outlook.com>
2024-07-22 18:48:21 -0400

    lesson 29 done

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,22 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="todo-wrapper">
+      <div class="todo-container">
+        <div class="todo-list">
+          <div class="todo-item">
+            <div class="todo-item-content">
+              <div class="todo-item-content-title">Walk the dog</div>
+              <div class="todo-item-content-description">Go to the forest near the zoo.</div>
+            </div>
+          </div>
+          <div class="todo-item">
+            <div class="todo-item-content">
+              <div class="todo-item-content-title">Buy some bread</div>
+              <div class="todo-item-content-description">Whole grain bread would be good.</div>
+            </div>
+          </div>
+          <div class="todo-item">
+            <div class="todo-item-content">
+              <div class="todo-item-content-title">Learn programming</div>
+              <div class="todo-item-content-description">Starting tomorrow would be best.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import playground from './playground.js';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  // function run automatically
+  created() {
+    playground();
+  },
+};
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +47,35 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.todo {
+  &-wrapper {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+
+  &-container {
+    width: 400px;
+    min-height: 400px;
+    background-color: #ededed;
+    border-radius: 8px;
+  }
+  &-item {
+    background-color: gray;
+    min-height: 70px;
+    margin: 10px;
+    padding: 10px;
+    color: white;
+    border-radius: 8px;
+    font-size: 2rem;
+    &-content-title {
+      font-weight: bold;
+    }
+    &-content-description {
+      font-size: 1.5rem;
+    }
+  }
 }
 </style>
